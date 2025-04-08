f3054234820e93c9233a6b4f1728795cc60f340e
ahravsoi <ahravsoi@outlook.com>
2025-04-07 21:40:59 -0400

    everything works except send message

--- chat_client.py
+++ chat_client.py
@@ -7,49 +7,199 @@ import threading
 
 class Client:
     def __init__(self, username, ip, port):
-        pass
+        self.server_address = (ip, port)
+        self.username = username
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connected = False
+        self.known_peers = {}  # Map of {username: (ip, listner_port)}
+        self.server_lock = threading.Lock()
 
-    def handle_message(self, message):
-        pass
+        # Create a seperate socket that will listen to incoming messages from other clients
+        self.client_listnener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_listnener.bind((ip, 0))  # Bind to any available port
+        self.client_listener_port = self.client_listnener.getsockname()[1] # Server should tell other clients to send messages to this port
+        self.client_listnener.listen(5)  # Listen for incoming connections
+        
 
-    def start(self):
-        pass
+    def connect_to_server(self):
+        try:
+            self.server_socket.connect(self.server_address)
+            self.connected = True
+            print(f'[*] Connected to server at {self.server_address}')
+    
+            self.login(self.username) # Send login request upon connection
+            threading.Thread(target=self.serverListener, daemon=True).start() # Starts a thread to receive messages from server
+            signal.signal(signal.SIGINT, self.logout)  # Handle Ctrl+C to logout FIX THIS LINE
+        except socket.error as e:
+            print(f'[*] Connection error: {e}')
+    
+    def message_server(self, packet):
+        try:
+            with self.server_lock:
+                self.server_socket.send(json.dumps(packet).encode('utf-8'))
+        except Exception as e:
+            print(f'[*] Error sending message to server: {e}')
 
-    def sendRequest(self, packet):
-        pass
+    def run(self):
+        '''
+            Start peer listener
+            Start the command line interface for the client to handle user inputted commands
+            Commands:
+                - LIST: List all users
+                - SEND <username> <message>: Send a message to a user
+            
+            Supports keyboard interrupts to logout
+        '''
+        threading.Thread(target=self.peerListener, daemon=True).start()  # Start listening for incoming messages from other clients
+        self.connect_to_server()  # Connect to the server
+        while self.connected: # CLI loop
+            try:
+                command = input("Please Enter command:\n")
+                command_parts = command.split(maxsplit=2)
+                if command_parts[0].lower() == 'list':
+                    rsp = self.handleLIST()
+                elif len(command_parts) == 3 and command_parts[0].lower() == 'send':
+                    destination_username = command_parts[1]
+                    message = command_parts[2]
+                    rsp = self.handleSEND(destination_username, message)
+                else:
+                    print("Invalid command. Please try again.")
+                
+                if rsp is not None:
+                    print(rsp)
+            except KeyboardInterrupt:
+                print("Logging out")
+                self.logout()
+            except Exception as e:
+                print(f'Error while listening for user commands: {e}')
+   
+    def serverListener(self):
+        '''
+            Continuously receive messages from the server
+        '''
+        while self.connected:
+            try:
+                data = self.server_socket.recv(1024)  # Receiving in chunks of 1024 bytes
+                if not data:
+                    print("[*] Server closed the connection.")
+                    self.connected = False
+                    break
+                message = json.loads(data.decode('utf-8'))
+                print(f'[*] Received message: {message}')
+
+                msg_type = message.get('type')
+                print(f'[*] Message type: {msg_type}')
+                if msg_type == 'RESPONSE':
+                    print(f'{message.get("message")}')
+                elif msg_type == 'ADDRESS':
+                    self.known_peers[message.get('requested_user_username')] = (message.get('requested_user_ip'), message.get('requested_user_listener_port'))
+                else :
+                    print(f'[*] Unknown message type: {msg_type}')
+                print("Please Enter Command:", end='\n', flush=True)
+
+            except Exception as e:
+                print(f'[*] Error receiving message: {e}')
+                self.connected = False
+                break
+        
+    def peerListener(self):
+        '''
+            Continuously receive connections from other clients on our listener socket.
+            When a connection is received, spawn a new thread to handle the connection.
+        '''
+        while True:
+            try:
+                conn, addr = self.client_listnener.accept()  # Accept incoming connections
+                threading.Thread(target=self.handle_peer_connection, args=(conn, addr), daemon=True).start()
+            except Exception as e:
+                print(f'[*] Error accepting connection: {e}')
+                break
+
+    def handle_peer_connection(self, conn, addr):
+        '''
+            Handle incoming connections from other clients.
+            This is where we will receive messages from other clients.
+
+            Only expect one type of message from the clients - MESSAGE
+        '''
+        try:
+            data = conn.recv(1024)  # Receiving in chunks of 1024 bytes
+            if not data:
+                print("[*] Peer closed the connection.")
+                return
+            message = json.loads(data.decode('utf-8'))
+            if message.get('type') == 'MESSAGE':
+                print(f'<From {message.get("source_ip")}:{message.get("source_port")}:{message.get("source_user")}> {message.get("message")} ')
+            else:
+                print(f'[*] Unknown message type: {message.get("type")}')
+        except Exception as e:
+            print(f'[*] Error receiving message from peer: {e}')
+        finally:
+            conn.close()
 
     def logout(self, signum=None, frame=None):
         packet = {
             "type": "SIGN-OUT",
             "username": self.username
         }
-        self.sendRequest(packet)
+        self.message_server(packet)
         self.running = False
-        self.close()
+        #self.close()
         sys.exit(0)
 
     def login(self, username):
         packet = {
             "type": "SIGN-IN",
-            "username": username
+            "username": username,
+            "reciever_port": self.client_listener_port
         }
-        self.sendRequest(packet)
+        self.message_server(packet)
 
     def handleLIST(self):
         packet = {
             "type": "LIST",
             "username": self.username
         }
-        self.sendRequest(packet)
+        self.message_server(packet)
 
     def handleSEND(self, dest_user, msg):
+        '''
+            Sends message to dest user. 
+        '''
         packet = {
             "type": "MESSAGE",
+            "source_ip": 0, # Need to figure out how to get the local ip address
+            "source_port": 0, # Need to figure out how to get the local port
             "source_user": self.username,
             "to": dest_user,
             "message": msg
         }
-        self.sendRequest(packet)
+        dest = self.get_destAddress(dest_user) # or request the address from the server
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as peer_socket:
+                peer_socket.connect(dest) # Open temporary socket to send the message
+                peer_socket.send(json.dumps(packet).encode('utf-8'))
+                print(f"Message sent to {dest_user} at {dest}")
+        except Exception as e:
+            print(f"Error sending message to {dest_user}. They may not be signed in: {e}")
+    
+    def get_destAddress(self, dest_user):
+        '''
+            Get the address of the destination user. If the destination user is not in the known_peers cache,
+            it will request the address from the server and wait until the peers table is updated.
+        '''
+        if dest_user in self.known_peers:
+            return self.known_peers[dest_user]
+        else:
+            packet = {
+                "type": "ADDRESS_REQUEST",
+                "source_user": self.username,
+                "requested_user": dest_user
+            }
+            self.message_server(packet)
+            while dest_user not in self.known_peers: # Wait for the local peers table to get updated
+                pass
+            return self.known_peers[dest_user]
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -58,5 +208,4 @@ if __name__ == '__main__':
     parser.add_argument('-sp', type=int, help='Server port number')
     args = parser.parse_args()
     client = Client(args.u, args.sip, args.sp)
-    client.start()
-    client.close()
+    client.run()
--- chat_server.py
+++ chat_server.py
@@ -3,6 +3,7 @@ import json
 import argparse
 import threading
 
+##TODO -  When someone logs out we should broadcast to all clients that they have logged out.
 class Server:
     def __init__(self, port, ip):
         self.ip = ip
@@ -13,7 +14,7 @@ class Server:
         self.server_socket.listen(5) # Can handle 5 clients at a time
         print(f'[*] Server started on {self.ip}:{self.port}')
         
-        self.knownClients = {} # Map of {username: (connection, address)}
+        self.knownClients = {} # Map of {username: (connection, address, receiver_port)}
         self.clients_lock = threading.Lock()
 
     def start(self):
@@ -42,14 +43,16 @@ class Server:
                 msg_type = message.get('type')
                 if msg_type == 'SIGN-IN':
                     username = message.get('username')
-                    rsp = self.handleSIGNIN(message, addr)
-                    with self.clients_lock:
-                        self.knownClients[username] = (conn, addr)
+                    rsp = self.handleSIGNIN(message, conn, addr)
                 elif msg_type == 'SIGN-OUT':
-                    pass
-
+                    rsp = self.handleSIGNOUT(message)
+                elif msg_type == 'LIST':
+                    rsp = self.handleLIST(message)
+                elif msg_type == 'ADDRESS_REQUEST':
+                    rsp = self.handleADDRESS_REQUEST(message)
                 if rsp is not None:
                     conn.send(json.dumps(rsp).encode('utf-8'))
+                    print(f' Sending {rsp} to {addr}')
 
         except json.JSONDecodeError:
             print(f'[*] Invalid JSON format from {addr}')
@@ -64,13 +67,14 @@ class Server:
             print(f'[*] Connection closed from {addr}')
                 
 
-    def handleSIGNIN(self, data, address):
+    def handleSIGNIN(self, data, conn, address):
         '''
             When a client signs in, save their username and address in the users dictionary.
             This will be useful when another clients request sthe destination of a user.
         '''
-        self.users[data['username']] = address
-        print(f'[*] User Signed In: {data["username"]} at {address}')
+        with self.clients_lock:
+            self.knownClients[data['username']] = (conn, address, data['reciever_port'])
+        print(f'[*] User Signed In: {data["username"]} at {address} recieves messages on port {data["reciever_port"]}')
         packet = {
             "type": "RESPONSE",
             "message": f'{data["username"]} Successfully Signed In'
@@ -81,7 +85,7 @@ class Server:
         '''
             Removes a client from the user dictionary to ensure consistency of the list command.
         '''
-        del self.users[data['username']]
+        del self.knownClients[data['username']]
         print(f'[*] User Signed Out: {data["username"]}')
         packet = {
             "type": "RESPONSE",
@@ -94,9 +98,11 @@ class Server:
             When a client requests a list of signed in users, return a list of all signed in users.
         '''
         print(f'[*] List Requested by {data["username"]}')
+        with self.clients_lock:
+            user_list = list(self.knownClients.keys())
         packet = {
             "type": "RESPONSE",
-            "message": 'Signed In Users: ' + ', '.join(self.users.keys())
+            "message": 'Signed In Users: ' + ', '.join(user_list)
         }
         return packet
 
@@ -105,23 +111,24 @@ class Server:
             When a client needs to send a message, they need to request the server for the location of that client.
             Return the address of the request user in the packet to be sent back to client.
         '''
-        if data['requested_user'] not in self.users:
-            print(f'[*] User {data["requested_user"]} not signed in.')
-            packet = {
-                "type": "RESPONSE",
-                "message": f'User {data["requested_user"]} is not signed in.'
-            }
-            return packet
-        print(f'[*] Address Rquested by {data["source_user"]} for {data["requested_user"]}')
-        packet = {
-            "type": "ADDRESS",
-            "username": data['requested_user'],
-            "address": self.users[data['requested_user']]
-        }
-        return packet
-    
-    def close(self):
-        pass
+        with self.clients_lock:
+            requested_user = data['requested_user']
+            if requested_user not in self.knownClients:
+                print(f'[*] User {requested_user} not signed in.')
+                packet = {
+                    "type": "RESPONSE",
+                    "message": f'User {requested_user} is not signed in.'
+                }
+                return packet
+            else:
+                print(f'[*] Address Rquested by {data["source_user"]} for {requested_user}')
+                packet = {
+                    "type": "ADDRESS",
+                    "requested_user_username": data['requested_user'], 
+                    "requested_user_ip": self.knownClients[requested_user][1][0], # IP address of the requested user I THINK DOUBLE CHECK
+                    "requested_user_listener_port": self.knownClients[requested_user][2], # Listening port of the requested user
+                }
+                return packet
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
